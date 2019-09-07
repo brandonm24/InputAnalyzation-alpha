074cc4d2993878bd4f93dd7faa2b9a8db10813fa
Brandon Mekler <brandonm@robo3d.com>
2019-09-06 19:59:09 -0700

    Add files via upload

--- InputAnalyze.py
+++ InputAnalyze.py
@@ -1,6 +1,7 @@
 import importlib
 import InputMathematics
 import numpy as numpy
+import re
 from PyDictionary import PyDictionary
 
 from ast import literal_eval
@@ -8,68 +9,85 @@ from ast import literal_eval
 def getDataType(inputData = "NOINPUT"):
     tempInput = str(inputData)
     tempReturn = ""
+    tempReturnARR = ""
     try:
-        if type(inputData) is numpy.ndarray:
+        if type(inputData) is numpy.ndarray: #fix this if branch
             if isinstance(inputData[0], int):
-                tempReturn += "NUMPYLIST,NUM," + tempInput
+                tempReturn += "NUMPYLIST,NUM"
+                return [tempReturn.split(","), inputData.tolist()]
             elif isinstance(inputData[0], str):
-                tempReturn += "NUMPYLIST,STR," + tempInput
+                tempReturn += "NUMPYLIST,STR"
+                return [tempReturn.split(","), inputData.tolist()]
             elif isinstance(inputData[0], numpy.ndarray):
                 tempReturn += "NUMPYMATRIX,"
                 if isinstance(inputData[0][0], int) or isinstance(inputData[0][0], float) or isinstance(inputData[0][0], complex):
-                    tempReturn += "NUM," + tempInput
+                    tempReturn += "NUM"
+                    return [tempReturn.split(","), inputData.tolist()]
                 elif isinstance(inputData[0][0], str):
-                    tempReturn += "STR," + tempInput
-            tempReturnARR = tempReturn.split(",")
-            index = 0
-            for string in tempReturnARR:
-                tempReturnARR[index] = string.replace("'[[","[[")
-                tempReturnARR[index] = string.replace(" ' ","")
-                tempReturnARR[index] = string.replace("\n ",", ")
-                index += 1
-            return tempReturnARR
+                    tempReturn += "STR"
+                    return [tempReturn.split(","), inputData.tolist()]
         elif isinstance(inputData,list):
             if isinstance(inputData[0], int) or isinstance(inputData[0], float) or isinstance(inputData[0], complex):
-                tempReturn += "LIST , NUM , " + tempInput
+                tempReturn += "LIST,NUM"
+                return [tempReturn.split(","), inputData]
             elif isinstance(inputData[0], str):
-                tempReturn += "LIST , STR , " + tempInput
+                tempReturn += "LIST,STR"
+                return [tempReturn.split(","), inputData]
             elif isinstance(inputData[0], list):
-                tempReturn += "MATRIX , "
+                tempReturn += "MATRIX,"
                 if isinstance(inputData[0][0], int) or isinstance(inputData[0][0], float) or isinstance(inputData[0][0], complex):
-                    tempReturn += "NUM , " + tempInput
+                    tempReturn += "NUM"
+                    return [tempReturn.split(","), inputData]
                 elif isinstance(inputData[0][0], str):
-                    tempReturn += "STR , " + tempInput
-            tempReturnARR = tempReturn.split(" , ")
-            index = 0
-            for string in tempReturnARR:
-                tempReturnARR[index] = string.replace("'","").replace("\n ", "")
-                index += 1
-            return tempReturnARR
+                    tempReturn += "STR"
+                    return [tempReturn.split(","), inputData]
+        elif len(inputData.split(" ")) > 1:
+            tempReturn += "STR"
+            return [tempReturn.split(","), inputData]
+        elif len(inputData.split(" ")) == 1:
+            tempReturn += "STR,WORD"
+            return [tempReturn.split(","), inputData]
         else:
             tempReturn = str(type(inputData))
             returnSTR1 = str(tempReturn).find("'")
             returnSTR2 = tempReturn[returnSTR1 + 1: -2] + ","+ tempInput
-            return returnSTR2.upper()
+            return returnSTR2.upper().split(",")
     except (ValueError, SyntaxError):
-        return str
-
-
-def inputInfo(inputData = "NOINPUT"):
-    if not inputData.any() or not inputData.all():
-        return "NOINPUT"
-    elif inputData.endswith(".exe") or inputData.endswith(".txt"):
-        return "FILE" + "," + str(inputData)
-    else:
-        returnSTR = str(getDataType(inputData))
-        returnSTR1 = str(returnSTR).find("'")
-        returnSTR2 = returnSTR[returnSTR1: -1]
-        return returnSTR2.replace("'","").upper() + "," + str(inputData)
+        return 0
 
 def inputWordCheck(inputData = "NOINPUT"):
-    dictionary = PyDictionary()
-    try:
-        tempOutput = dictionary.meaning(inputData, disable_errors=True)
-    except:
+    if len(inputData.split(" ")) > 1:
         return 0
+    elif len(inputData.split(" ")) == 1:
+        dictionary = PyDictionary()
+        tempOutput = dictionary.meaning(inputData)
+        if tempOutput is 0:
+            return 0
+        else:
+            return 1
     else:
-        return 1
+        return 0
+
+
+def getWordDefinition(inputData = "NOINPUT"):
+    dictionary = PyDictionary()
+    tempDict = dictionary.meaning(inputData)[next(iter(dictionary.meaning(inputData)))]
+    index = 0
+    regex = re.compile('[@_!#$%^&*()<>?/\|}{~:]')
+    for str in tempDict:
+        tempDict[index] = re.sub(regex, '', str)
+        index += 1
+    return tempDict
+
+def getWordPartOfSpeech(inputData = "NOINPUT"):
+    dictionary = PyDictionary()
+    return next(iter(dictionary.meaning(inputData))).upper()
+
+def numpyARRClean(numpyARR):
+    return numpyARR.replace("\n ",", ")
+
+def analyzeInput(inputData = "NOINPUT"):
+    if inputWordCheck(inputData) == 0:
+        return getDataType(inputData)
+    else:
+        return [getDataType(inputData),getWordDefinition(inputData),getWordPartOfSpeech(inputData)]
--- main.py
+++ main.py
@@ -2,10 +2,12 @@ import importlib
 from importlib import reload
 import InputAnalyze
 import InputMathematics
-import numpy as nump
-from InputAnalyze import inputInfo
+import numpy as numpy
 from InputAnalyze import getDataType
 from InputAnalyze import inputWordCheck
+from InputAnalyze import getWordDefinition
+from InputAnalyze import getWordPartOfSpeech
+from InputAnalyze import analyzeInput
 reload(InputAnalyze)
 from InputMathematics import NumericMatrixInfo
 from InputMathematics import NumericMatrixComparison
@@ -16,7 +18,10 @@ reload(InputMathematics)
 
 
 
-a = [[2,2,4,5,6,67,8,6],[1,2,3]]
-print(inputWordCheck("greetings"))
-print(getDataType("greetings"))
+a = [[1,2,4,5,6,67,8,6],[1]]
+#print(inputWordCheck("zebras are"))
+# print(getDataType("salamander"))
+# print(getWordDefinition("salamander"))
+# print(getWordPartOfSpeech("salamander"))
 #print(NumericMatrixInfo([[1,2,4,5,6,67,8,6],[2,2,4,5,6,67,8,6],[1,2,3]]))
+print(analyzeInput("good"))
